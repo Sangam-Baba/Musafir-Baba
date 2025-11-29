0e1b8c983261b6c02e77f36449677b1eec9edd2c
Sangam-Baba <sangam.gupta@musafirbaba.com>
2025-11-29 11:04:39 +0530

    Make responsive of Video section

--- frontend/src/components/custom/HomeVideoBanner.tsx
+++ frontend/src/components/custom/HomeVideoBanner.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   Carousel,
   CarouselContent,
@@ -9,54 +10,68 @@ import {
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 import { VideoBannerType } from "@/components/admin/VideoBannerList";
-export const getAllMedia = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/videobanner`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  if (!res.ok) throw new Error("Failed to fetch media");
-  const data = await res.json();
-  return data?.data;
-};
+import Image from "next/image";
+
 function HomeVideoBanner({ data }: { data: VideoBannerType[] }) {
   // const mediaVideo = await getAllMedia();
-  return (
-    <div className="w-full max-w-7xl mx-auto">
-      <Carousel
-        opts={{ loop: true }}
-        plugins={[Autoplay({ delay: 5000, stopOnInteraction: false })]}
-        className="w-full"
-      >
-        <CarouselContent>
-          {data?.map((item: VideoBannerType, index: number) => (
-            <CarouselItem key={index}>
-              <a
-                href={item.link}
-                className="relative w-full overflow-hidden rounded-lg"
-              >
-                <video
-                  className="w-full h-full object-cover rounded-xl"
-                  autoPlay
-                  loop
-                  muted
-                  playsInline
-                  preload="none"
-                  controls={false}
-                  poster={item.media?.thumbnail_url}
-                  style={{ aspectRatio: "553/150" }} // KEY
-                >
-                  <source src={item.media?.url} />
-                </video>
+  const ref = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
 
-                <CarouselPrevious className="absolute left-3 top-1/2 -translate-y-1/2 z-20" />
-                <CarouselNext className="absolute right-3 top-1/2 -translate-y-1/2 z-20" />
-              </a>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-      </Carousel>
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => entry.isIntersecting && setIsVisible(true),
+      { threshold: 0.1 }
+    );
+    if (ref.current) observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, []);
+  return (
+    <div ref={ref} className="w-full max-w-7xl mx-auto">
+      {!isVisible && (
+        <div className="w-full h-[200px] bg-gray-400 aspect-w-553 aspect-h-200">
+          <Image
+            src={data?.[0]?.media?.thumbnail_url}
+            fill
+            alt="h1"
+            className="rounded-2xl w-full"
+          />
+        </div>
+      )}
+      {isVisible && (
+        <Carousel
+          opts={{ loop: true }}
+          plugins={[Autoplay({ delay: 5000, stopOnInteraction: false })]}
+          className="w-full"
+        >
+          <CarouselContent>
+            {data?.map((item: VideoBannerType, index: number) => (
+              <CarouselItem key={index}>
+                <a
+                  href={item.link}
+                  className="relative w-full overflow-hidden rounded-lg"
+                >
+                  <video
+                    className="w-full h-full object-cover rounded-xl"
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
+                    preload="none"
+                    controls={false}
+                    poster={item.media?.thumbnail_url}
+                    style={{ aspectRatio: "553/200" }} // KEY
+                  >
+                    <source src={item.media?.url} />
+                  </video>
+
+                  <CarouselPrevious className="absolute left-3 top-1/2 -translate-y-1/2 z-20" />
+                  <CarouselNext className="absolute right-3 top-1/2 -translate-y-1/2 z-20" />
+                </a>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      )}
     </div>
   );
 }
