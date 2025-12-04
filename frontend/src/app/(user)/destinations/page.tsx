b56150aaae5cf5ea8c5d783ae295b4e9f6acad20
Sangam-Baba <sangam.gupta@musafirbaba.com>
2025-12-04 13:37:40 +0530

    Add Search at Destination

--- frontend/src/app/(user)/destinations/page.tsx
+++ frontend/src/app/(user)/destinations/page.tsx
@@ -1,13 +1,8 @@
-import Hero from "@/components/custom/Hero";
-import { Card, CardContent } from "@/components/ui/card";
-import Image from "next/image";
-import Link from "next/link";
 import { notFound } from "next/navigation";
 import React from "react";
 import { Metadata } from "next";
-// import { getCategory } from "../holidays/PackagesClient";
-// import { getDestinationSeo } from "@/components/admin/DestinationSeoNew";
-interface Destination {
+import DestinationPageClient from "./pageClient";
+export interface DestinationInterface {
   _id: string;
   name: string;
   country: string;
@@ -32,68 +27,15 @@ export function generateMetadata(): Metadata {
 
 const getAllDestinations = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/destination`, {
-    next: { revalidate: 86400 },
+    next: { revalidate: 120 },
   });
   if (!res.ok) return notFound();
   const data = await res.json();
   return data?.data;
 };
 async function page() {
-  const destinations = await getAllDestinations();
-  //   const res = await getCategory();
-  //   const category = res?.data;
-
-  //   const destinationSeo = await getDestinationSeo("", "");
-  return (
-    <section className="flex flex-col">
-      <div>
-        <Hero
-          image="/Heroimg.jpg"
-          title="Destinations"
-          align="center"
-          height="lg"
-          overlayOpacity={100}
-        />
-      </div>
-
-      <div className="max-w-7xl mx-auto py-16 px-6">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-8 mt-6">
-          {destinations.map((destination: Destination, i: number) => (
-            <Link key={i} href={`/destinations/${destination.state}`}>
-              <Card
-                className=" pt-0
-            group overflow-hidden rounded-2xl border 
-            shadow-md hover:shadow-xl 
-            transition-all duration-500
-            hover:-translate-y-1 cursor-pointer
-          "
-              >
-                <div className="relative overflow-hidden">
-                  <Image
-                    src={destination?.coverImage?.url ?? "/Heroimg.jpg"}
-                    alt={destination?.coverImage?.alt ?? "Destination image"}
-                    width={200}
-                    height={500}
-                    className="
-                w-full h-32 object-cover 
-                transition-transform duration-700
-                group-hover:scale-110
-              "
-                  />
-                </div>
-
-                <CardContent className="">
-                  <p className="text-sm font-semibold text-center tracking-wide">
-                    {destination?.name}
-                  </p>
-                </CardContent>
-              </Card>
-            </Link>
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+  const destinations = (await getAllDestinations()) as DestinationInterface[];
+  return <DestinationPageClient destinations={destinations} />;
 }
 
 export default page;
---
+++ frontend/src/app/(user)/destinations/pageClient.tsx
@@ -0,0 +1,108 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Hero from "@/components/custom/Hero";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import Link from "next/link";
+import { DestinationInterface } from "@/app/(user)/destinations/page";
+import { Search } from "lucide-react";
+function DestinationPageClient({
+  destinations,
+}: {
+  destinations: DestinationInterface[];
+}) {
+  const [filter, setFilter] = useState("");
+  const [filteredDestinations, setFilteredDestinations] =
+    useState(destinations);
+
+  useEffect(() => {
+    const filtered = destinations.filter(
+      (destination: DestinationInterface) => {
+        return (
+          destination.country.toLowerCase().includes(filter.toLowerCase()) ||
+          destination.state.toLowerCase().includes(filter.toLowerCase()) ||
+          destination.name.toLowerCase().includes(filter.toLowerCase())
+        );
+      }
+    );
+    setFilteredDestinations(filtered);
+  }, [filter, destinations]);
+  return (
+    <section className="flex flex-col">
+      <div className="relative">
+        <Hero
+          image="/Heroimg.jpg"
+          title="Destinations"
+          align="center"
+          height="lg"
+          overlayOpacity={100}
+        />
+        <div className="z-10 absolute bottom-6 left-1/2 -translate-x-1/2 w-[90%] md:w-[50%] lg:w-[35%]">
+          <div
+            className="
+      flex items-center gap-3
+      backdrop-blur-md bg-white/15 
+      border border-[#FE5300]/50 
+      shadow-lg rounded-full
+      px-6 py-3 
+      transition-all duration-300
+      hover:bg-white/25
+    "
+          >
+            <Search className="w-6 h-6 text-white" />
+
+            <input
+              placeholder="Search city, state or country..."
+              onChange={(e) => setFilter(e.target.value)}
+              className="
+        flex-1 text-white text-lg bg-transparent border-none 
+        placeholder:text-white/70 focus:ring-0 focus:outline-none
+      "
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="max-w-7xl mx-auto py-16 px-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-8 mt-6">
+          {filteredDestinations.map(
+            (destination: DestinationInterface, i: number) => (
+              <Link key={i} href={`/destinations/${destination.state}`}>
+                <Card
+                  className=" pt-0
+            group overflow-hidden rounded-2xl border 
+            shadow-md hover:shadow-xl 
+            transition-all duration-500
+            hover:-translate-y-1 cursor-pointer
+          "
+                >
+                  <div className="relative overflow-hidden">
+                    <Image
+                      src={destination?.coverImage?.url ?? "/Heroimg.jpg"}
+                      alt={destination?.coverImage?.alt ?? "Destination image"}
+                      width={200}
+                      height={500}
+                      className="
+                w-full h-32 object-cover 
+                transition-transform duration-700
+                group-hover:scale-110
+              "
+                    />
+                  </div>
+
+                  <CardContent className="">
+                    <p className="text-sm font-semibold text-center tracking-wide">
+                      {destination?.name}
+                    </p>
+                  </CardContent>
+                </Card>
+              </Link>
+            )
+          )}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default DestinationPageClient;
