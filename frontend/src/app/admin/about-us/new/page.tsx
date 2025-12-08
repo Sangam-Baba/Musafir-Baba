df7e17c7d0c807d9a4f374e656e99a50324f9d26
Sangam-Baba <sangam.gupta@musafirbaba.com>
2025-12-08 12:01:33 +0530

    Make about-us page CSR

--- frontend/src/app/admin/about-us/new/page.tsx
+++ frontend/src/app/admin/about-us/new/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 export const dynamic = "force-dynamic";
-export const revalidate = 0;
 export const fetchCache = "force-no-store";
 import React from "react";
 import { useMutation } from "@tanstack/react-query";
