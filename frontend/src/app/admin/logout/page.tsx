39bbc8bd8ad174503a8b3bbf8085cda607ddd513
Sangam-Baba <sangam.gupta@musafirbaba.com>
2025-12-08 10:42:30 +0530

    Make logout in Suspance

--- frontend/src/app/admin/logout/page.tsx
+++ frontend/src/app/admin/logout/page.tsx
@@ -1,55 +1,12 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useAdminAuthStore } from "@/store/useAdminAuthStore";
-import { toast } from "sonner";
+import { Suspense } from "react";
+import LogoutAdminClient from "@/components/auth/LogoutAdmin";
 
 export default function LogoutAdmin() {
-  const router = useRouter();
-  const params = useSearchParams();
-  const token = useAdminAuthStore((s) => s.accessToken) as string;
-
-  const redirect = params.get("redirect") || "/";
-  const logout = useAdminAuthStore((s) => s.logout);
-
-  useEffect(() => {
-    (async () => {
-      try {
-        await logout(token);
-        toast.success("Logged out");
-        router.replace(redirect);
-      } catch (err) {
-        toast.error("Logout failed");
-        router.replace(redirect);
-      }
-    })();
-  }, [logout, redirect, router, token]);
-
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <svg
-        className="animate-spin h-8 w-8 mb-4"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        aria-hidden="true"
-      >
-        <circle
-          className="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          strokeWidth="4"
-        ></circle>
-        <path
-          className="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-        ></path>
-      </svg>
-      <p className="text-sm text-gray-700">Logging out…</p>
-    </div>
+    <Suspense fallback={<div>Loading…</div>}>
+      <LogoutAdminClient />
+    </Suspense>
   );
 }
