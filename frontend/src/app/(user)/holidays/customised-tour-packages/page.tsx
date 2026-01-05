cbf1132af84817de9b6bee94a5d4ef353d8e8947
Sangam-Baba <sangam.gupta@musafirbaba.com>
2026-01-05 11:22:40 +0530

    Fix Meta at Customised category

--- frontend/src/app/(user)/holidays/customised-tour-packages/page.tsx
+++ frontend/src/app/(user)/holidays/customised-tour-packages/page.tsx
@@ -6,6 +6,7 @@ import CustomizedTourClient from "./CustomizedTourClient";
 import { getBreadcrumbSchema } from "@/lib/schema/breadcrumb.schema";
 import { getCollectionSchema } from "@/lib/schema/collection.schema";
 import Script from "next/script";
+import { title } from "process";
 interface Plan {
   title: string;
   include: string;
@@ -55,7 +56,10 @@ export const getAllCustomizedPackages = async () => {
 };
 
 export async function generateMetadata() {
-  return {
+  const res = await getCategoryBySlug("customised-tour-packages");
+  const { category } = res?.data ?? {};
+
+  const meta = {
     title: "Customised Tour Packages – Tailor-Made Holidays for Every Traveler",
     description:
       "Explore customised tour packages designed around your interests, budget, and travel style. From luxury getaways to family vacations, we plan every detail so you can enjoy a truly personalised holiday experience.",
@@ -80,6 +84,39 @@ export async function generateMetadata() {
       type: "website",
     },
   };
+
+  if (!category) return meta;
+  return {
+    title: category.metaTitle,
+    description: category.metaDescription,
+    keywords: category?.keywords,
+    alternates: {
+      canonical: `https://musafirbaba.com/holidays/customised-tour-packages`,
+    },
+    openGraph: {
+      title: category.metaTitle,
+      description: category.metaDescription,
+      url: `https://musafirbaba.com/holidays/customised-tour-packages`,
+      images: [
+        {
+          url:
+            category?.coverImage.url ||
+            "https://musafirbaba.com/homebanner.webp",
+          width: 1200,
+          height: 630,
+          alt: category.name,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: category.metaTitle,
+      description: category.metaDescription,
+      images: [
+        category?.coverImage.url || "https://musafirbaba.com/homebanner.webp",
+      ],
+    },
+  };
 }
 
 async function CustomizedPackagePage() {
@@ -98,7 +135,7 @@ async function CustomizedPackagePage() {
   return (
     <section className="w-full mb-12">
       <Hero
-        image={AllPackages[0]?.coverImage?.url || "/Hero1.jpg"}
+        image={category?.coverImage?.url || "/Hero1.jpg"}
         title={category?.name}
         description={category?.description}
         align="center"
